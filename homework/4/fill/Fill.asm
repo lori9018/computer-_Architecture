209b2dc14d73fa3256b102290c74fb30a1186f57
lori9018 <arther20059018@gmail.com>
2025-12-15 21:53:45 +0800

    04finish

--- homework/4/fill/Fill.asm
+++ homework/4/fill/Fill.asm
@@ -8,4 +8,54 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+    @KBD        // Load Keyboard memory address (24576)
+    D=M         // Read the value at that address
+    @ON
+    D;JGT       // If value > 0 (key is pressed), jump to (ON)
+    
+    @OFF
+    0;JMP       // Else (value == 0), jump to (OFF)
+
+(ON)
+    @-1         // 1111111111111111 in binary (Black)
+    D=A         // Store -1 (as a value, though actually A holds 65535, we use it as data)
+                // Note: In Hack ASM 'M=-1' or 'D=-1' is safer, but let's use a variable approach
+    D=-1        // Explicitly set D to all 1s
+    @color
+    M=D         // Save 'black' to the color variable
+    @DRAW
+    0;JMP       // Jump to the drawing logic
+
+(OFF)
+    @0          // 0000000000000000 in binary (White)
+    D=A
+    @color
+    M=D         // Save 'white' to the color variable
+    @DRAW
+    0;JMP       // Jump to the drawing logic
+
+(DRAW)
+    @SCREEN     // Load Screen memory start address (16384)
+    D=A
+    @ptr        // Initialize a pointer variable
+    M=D
+
+(NEXT_PIXEL)
+    @ptr        // Get current pixel address
+    D=M
+    @24576      // Load Keyboard address (which is Screen End + 1)
+    D=D-A       // Calculate (Current Address - End Address)
+    @LOOP
+    D;JEQ       // If result is 0 (we reached the end), go back to main LOOP
+
+    @color      // Load the chosen color (0 or -1)
+    D=M
+    @ptr        // Load the address of the pixel pointer
+    A=M         // Set A-register to the address stored in ptr
+    M=D         // Paint the pixels at that address
+
+    @ptr
+    M=M+1       // Increment the pixel pointer to the next word
+    @NEXT_PIXEL
+    0;JMP       // Continue the drawing loop
--- homework/4/mult/Mult.asm
+++ homework/4/mult/Mult.asm
@@ -7,4 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@R2     // Select register R2
+    M=0     // Initialize R2 (result) to 0
+
+    @R1     // Select register R1
+    D=M     // Load value of R1 into D
+    @i      // Select variable i (counter)
+    M=D     // i = R1
+
+(LOOP)
+    @i      // Select i
+    D=M     // Load current value of i into D
+    @END    // Select END label
+    D;JEQ   // If i == 0, jump to END
+
+    @R0     // Select register R0
+    D=M     // Load value of R0 into D
+    @R2     // Select R2
+    M=D+M   // R2 = R2 + R0 (Add R0 to current total)
+
+    @i      // Select i
+    M=M-1   // Decrement counter i
+
+    @LOOP   // Select LOOP label
+    0;JMP   // Jump back to start of loop
+
+(END)
+    @END    // Select END label
+    0;JMP   // Infinite loop to terminate program safely
